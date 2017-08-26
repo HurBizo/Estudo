17eafaaf963620aaeede1d5000b79450f79c0764
LenilsonSousa <31359192+HurBizo@users.noreply.github.com>
2017-08-25 22:36:09 -0300

    Update calculo.php

--- calculo.php
+++ calculo.php
@@ -12,23 +12,43 @@
 			
 			break;
 			
-		case 'Multiplicar':
+		case 'Multiplicacao':
 			$mult = n1 * n2;
 			echo"$mult";
 			
 			break;
 			
-		case 'Dividir':
+		case 'Dividisao':
 			$div = n1 / n2;
 			echo"$div";
 			
 			break;
 			
-		case 'Subtrair':
+		case 'Subtracao':
 			$sub = n1 - n2;
 			echo"$sub";
 			
 			break;
+			
+		case 'RaizQuadrada':
+			$raiz = sqrt($v1);
+			echo"$raiz";
+			
+			break;
+				
+		case 'Exponenciacao':
+			$exponen = pow($v1, $v2);
+			echo"exponen";
+			
+			break;
+		
+		case 'Resto':
+			$resto = $v1 % $v2;
+			echo"resto";
+			
+			break;
+			
+		
 		default:
 	}
 ?>
