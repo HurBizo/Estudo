6bfca9f18b092f74d0bc1fa02bdbd84b596b0b4a
Lenilson Sousa <31359192+HurBizo@users.noreply.github.com>
2017-08-26 22:31:26 -0300

    Update calculo.php

--- calculo.php
+++ calculo.php
@@ -5,27 +5,27 @@
 	$op = $_GET ['op'];
 	
 		
-	switch(op){
+	switch($op){
 		case 'Somar':
-			$soma = n1 + n2;
+			$soma = $n1 + $n2;
 			echo"$soma";
 			
 			break;
 			
 		case 'Multiplicacao':
-			$mult = n1 * n2;
+			$mult = $n1 * $n2;
 			echo"$mult";
 			
 			break;
 			
 		case 'Dividisao':
-			$div = n1 / n2;
+			$div = $n1 / $n2;
 			echo"$div";
 			
 			break;
 			
 		case 'Subtracao':
-			$sub = n1 - n2;
+			$sub = $n1 - $n2;
 			echo"$sub";
 			
 			break;
