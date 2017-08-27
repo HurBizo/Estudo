2b4514ad140e1bb48ae72396f1b3ba90934ec609
Lenilson Sousa <31359192+HurBizo@users.noreply.github.com>
2017-08-26 23:11:24 -0300

    Create calendario.php

---
+++ PHP/calendario.php
@@ -0,0 +1,44 @@
+<?php
+	
+	$op = $_GET ['op'];
+	
+		
+	switch($op){
+		case '1':
+			echo"Tem aula!";
+			
+			break;
+			
+		case '2':
+			echo"Tem aula!";
+			
+			break;
+			
+		case '3':
+			echo"Tem aula!";
+			
+			break;
+			
+		case '4':
+			echo"Tem aula!";
+			
+			break;
+			
+		case '5':
+			echo"Tem aula!";
+			
+			break;
+
+		case '6':
+			echo"Não tem aula!";
+			
+			break;
+		
+	  case '7':
+			echo"Não tem aula!";
+			
+			break;
+		
+		default:
+	}
+?>
