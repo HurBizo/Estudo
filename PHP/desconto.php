bdde594e9251c6ca85453c106b0d86056ca1a771
Smile <31359192+HurBizo@users.noreply.github.com>
2017-08-28 20:01:15 -0300

    Update desconto.php

--- PHP/desconto.php
+++ PHP/desconto.php
@@ -7,15 +7,15 @@
       
       if($compra <= 200){
         $desconto = $compra * 0.1;
-        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>"
-        echo"<h1>O valor da compra será: R$$total$osma !</h1>"
+        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>";
+        echo"<h1>O valor da compra será: R$$total$osma !</h1>";
       }else if($compra < 500){
         $desconto = $compra * 0.15;
-        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>"
-        echo"<h1>O valor da compra será: R$$total$osma !</h1>"
+        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>";
+        echo"<h1>O valor da compra será: R$$total$osma !</h1>";
       }else if($compra > 500){
         $desconto = $compra * 0.2;
-        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>"
-        echo"<h1>O valor da compra será: R$$total$osma !</h1>"
+        echo"<h2>Olá $nome, sua compra de R$ $compra$osma sofreu desconto de R$$desconto$osma!</h2> <br/>";
+        echo"<h1>O valor da compra será: R$$total$osma !</h1>";
       }
 ?>
