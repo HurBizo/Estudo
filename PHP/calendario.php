ce71fd4d0d34e99dc26235235ea3996cf5df92e7
Smile <31359192+HurBizo@users.noreply.github.com>
2017-08-28 20:17:36 -0300

    Update calendario.php

--- PHP/calendario.php
+++ PHP/calendario.php
@@ -4,37 +4,37 @@
 	
 		
 	switch($op){
-		case '1':
-			echo"Tem aula!";
+		case 1:
+			echo"<h1> | Programação para computador | "."<br/> "." |Programação para computador| ";
 			
 			break;
 			
-		case '2':
-			echo"Tem aula!";
+		case 2:
+			echo"<h1> | Programação para computador | "."<br/> "." |Programação para computador| ";
 			
 			break;
 			
-		case '3':
-			echo"Tem aula!";
+		case 3:
+			echo"<h1> | Programação para computador | "."<br/> "." |Programação para computador| ";
 			
 			break;
 			
-		case '4':
-			echo"Tem aula!";
+		case 4:
+			echo"<h1> | Programação para computador | "."<br/> "." |Programação para computador| ";
 			
 			break;
 			
-		case '5':
-			echo"Tem aula!";
+		case 5:
+			echo"<h1> | Programação para computador | "."<br/> "." |Programação para computador| ";
 			
 			break;
 
-		case '6':
+		case 6:
 			echo"Não tem aula!";
 			
 			break;
 		
-	  case '7':
+	  	case 7:
 			echo"Não tem aula!";
 			
 			break;
