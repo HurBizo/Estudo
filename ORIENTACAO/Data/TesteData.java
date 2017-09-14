8eb389122b01203fb5ddfebabd05e71a054c3a7e
Smile <31359192+HurBizo@users.noreply.github.com>
2017-09-13 22:39:18 -0300

    Update TesteData.java

--- ORIENTACAO/Data/TesteData.java
+++ ORIENTACAO/Data/TesteData.java
@@ -1,18 +1,25 @@
-
 package pct;
-
+import java.util.Scanner;
 
 public class TesteData {
     public static void main(String[] args) {
+        int d = 0, m = 0, a = 0;
+        
         //Criação de instancia de um objeto da classe data
         Data data = new Data();
-        
+       
         //construção do objeto data
-        data.dia = 1;
-        data.mes = 1;
-        data.ano = 2017;
+        //data.dia = 1; Obsoleto
+        //data.mes = 1;
+        //data.ano = 2017;
+        Scanner entrada = new Scanner(System.in);
+        
+        data.setDia(d);
+        data.setMes(m);
+        data.setAno(a);
         
         //imprimeindo uma data
+       
         System.out.println("Data: " + data.imprimirData());
     }
 }
