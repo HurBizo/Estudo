a2e402ce3971acc8dba361ce9ff80178b04a1bbd
Smile <31359192+HurBizo@users.noreply.github.com>
2017-11-13 21:14:24 -0200

    Create Produto.java

---
+++ JAVA/Produto/Produto.java
@@ -0,0 +1,13 @@
+package pct;
+
+public class Produto {
+    String descricao;
+    double preco;
+    
+    public void imprimirProduto(){
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Preço: " + preco);
+        }
+    
+    
+}
